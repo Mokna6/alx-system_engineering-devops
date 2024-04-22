ba76fc0c4d59a4b04c250920ccfc4ad70c558cbb
mohammed <eng.saber.ms@gmail.com>
2024-04-22 12:26:35 -0700

    FXg

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,13 +6,13 @@ import sys
 
 if __name__ == '__main__':
     user = sys.argv[1]
-    url_user = 'https://moknaplaceholder.typicode.com/users/' + user
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
     res = requests.get(url_user)
     """ANYTHING"""
-    user_name = res.mokna().get('username')
+    user_name = res.json().get('username')
     task = url_user + '/todos'
     res = requests.get(task)
-    tasks = res.mokna()
+    tasks = res.json()
 
     with open('{}.csv'.format(user), 'w') as csvfile:
         for task in tasks:

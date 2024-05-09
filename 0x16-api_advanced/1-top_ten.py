9d5a97307f2579071864ad983f6797d803eaaba0
mohammed <eng.saber.ms@gmail.com>
2024-05-08 19:14:09 -0700

    D

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+""" top 10 posts"""
+
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    """get top 10"""
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    res = requests.get(url, headers={'User-Agent': 'AyaTarek95'},
+                       allow_redirects=False)
+
+    if res.status_code == 200:
+        posts = res.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post.get('data', {}).get('title', ''))
+    else:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""Contains recurse function"""
+""" top 10 posts"""
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        return None
+def top_ten(subreddit):
+    """get top 10"""
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    res = requests.get(url, headers={'User-Agent': 'AyaTarek95'},
+                       allow_redirects=False)
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_listi
+    if res.status_code == 200:
+        posts = res.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post.get('data', {}).get('title', ''))
+    else:
+        print(None)
